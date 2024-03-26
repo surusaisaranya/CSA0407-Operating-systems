143c4aa5476ccc6194d7fd3b1be782f898e69c05
surusaisaranya <125387475+surusaisaranya@users.noreply.github.com>
2024-03-26 13:24:59 +0530

    Add files via upload

---
+++ PROGRAM 27 UNIX COMMAND.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main() {
+    DIR *directory;
+    struct dirent *entry;
+
+    
+    directory = opendir("C:\\Users\\chait\\OneDrive\\Documents");
+
+    if (directory == NULL) {
+        perror("Unable to open directory");
+        return EXIT_FAILURE;
+    }
+
+    while ((entry = readdir(directory)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+
+   
+    closedir(directory);
+
+    return EXIT_SUCCESS;
+}
+
