762ede47a46774028a735abe6aedc710d8967b57
surusaisaranya <125387475+surusaisaranya@users.noreply.github.com>
2024-03-30 16:53:09 +0530

    Update PROGRAM 14 SINGLE LEVEL DIRECTORY.cpp

--- PROGRAM 14 SINGLE LEVEL DIRECTORY.cpp
+++ PROGRAM 14 SINGLE LEVEL DIRECTORY.cpp
@@ -1,37 +1,13 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-int main()
-{
-int nf=0,i=0,j=0,ch;
-char mdname[10],fname[10][10],name[10];
-printf("Enter the directory name:");
-scanf("%s",mdname);
-printf("Enter the number of files:");
-scanf("%d",&nf);
-do
-{
-printf("Enter file name to be created:");
-scanf("%s",name);
-for(i=0;i<nf;i++)
-{
-if(!strcmp(name,fname[i]))
-break;
-}
-if(i==nf)
-{
-strcpy(fname[j++],name);
-nf++;
-}
-else
-printf("There is already %s\n",name);
-printf("Do you want to enter another file(yes - 1 or no - 0):");
-scanf("%d",&ch);
-}
-while(ch==1);
-printf("Directory name is:%s\n",mdname);
-printf("Files names are:");
-for(i=0;i<j;i++)
-printf("\n%s",fname[i]);
-getch();
+#include <stdio.h>
+
+int main() {
+    FILE *file_ptr = fopen("hi saranya.txt", "w"); // Create file in a single-level directory
+    if (file_ptr == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    fprintf(file_ptr, "This is a file organized using a single-level directory.\n");
+    fclose(file_ptr);
+    printf("File 'file.txt' created and written successfully in the 'data' directory!\n");
+    return 0;
 }
